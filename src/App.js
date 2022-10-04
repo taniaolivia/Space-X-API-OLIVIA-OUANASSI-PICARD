a92b700372e970379e03ff141726f2e32dfa5720
taniaolivia <taniaolivia@macbook-pro-de-tania.home>
2022-10-04 17:41:27 +0200

    Add capsules component in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Launches from './components/Launches';
+import Capsules from './components/Capsules';
+
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <Launches/>
+      <Capsules/>
     </div>
   );
 }
