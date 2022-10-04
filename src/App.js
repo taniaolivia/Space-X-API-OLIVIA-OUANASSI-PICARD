69f81f8a7e5208c6197481a661c5b0361080b9bf
taniaolivia <taniaolivia@macbook-pro-de-tania.home>
2022-10-04 17:58:06 +0200

    Add routes for capsules and launches

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import Launches from './components/Launches';
 import Capsules from './components/Capsules';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Launches/>
-      <Capsules/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <a href="/launches"><h1>Launches</h1></a>
+              <a href="/capsules"><h1>Capsules</h1></a>
+            </div>
+          }/>
+          <Route path="/launches" element={<Launches/>}/>
+          <Route path="/capsules" element={<Capsules/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
