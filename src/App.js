2bef5adb99304b7c7513d1adb0afab0645872984
Rouissi Meriam <meriam.rouissi@etu.univ-orleans.fr>
2022-10-04 17:05:00 +0200

    display_capsules_done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import api from "./api/Api.js";
+
+
+
+
+ class Capsules extends React.Component{
+  state={
+    capsules:[]
+  }
+  componentDidMount() {
+    api.get(`capsules`)
+      .then(res => {
+        const capsules = res.data;
+        this.setState({ capsules });
+      })
+  }
+
+  render() {
+    return (
+      <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th rowSpan={2}>capsule_id</th>
+                        <th rowSpan={2}>capsule_serial </th>
+                        <th rowSpan={2}>status</th>
+                        <th rowSpan={2}>mission</th>
+                       
+                    </tr>
+                    <tr>
+                        <th>landings</th>
+                        <th>type</th>
+                        <th>reuse_count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.capsules.map(capsule =>
+                            <tr key={capsule.capsule_id}> 
+                                <th>{capsule.capsule_serial}</th>
+                                <td>{capsule.status}</td>
+                                <td>{capsule.mission}</td>
+                                <td>{capsule.landings}</td>
+                                <td>{capsule.type}</td>
+                                <td>{capsule.reuse_count}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+    )
+  }
+
+}
+
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Capsules/>
+      
     </div>
   );
 }
