b65d4341a9dde165fc4e70e8d7764bcc9bee8539
Kilian Pichard <kil.pichard@icloud.com>
2022-10-05 10:05:39 +0200

    Change Components to hooks

--- src/components/Capsules.js
+++ src/components/Capsules.js
@@ -1,53 +1,49 @@
-import React from "react";
+import React, {useEffect} from "react";
 import api from "../api/Api.js";
 
-export default  class Capsules extends React.Component{
-    state={
-      capsules:[]
-    }
-    componentDidMount() {
-      api.get(`capsules`)
-        .then(res => {
-          const capsules = res.data;
-          this.setState({ capsules });
-        })
-    }
+export default function Capsules() {
+ 
+  const [capsules, setCapsules] = React.useState([]);
+
+  useEffect(() => {
+    api.get(`capsules`).then((res) => {
+      const capsules = res.data;
+      setCapsules(capsules);
+    });
+  }, []);
   
-    render() {
-      return (
-        <table className="table table-bordered">
-                  <thead>
-                    <tr>
-                        <th colSpan={7}>CAPSULES</th>
-                    </tr>
-                      <tr>
-                          <th rowSpan={2}>capsule_id</th>
-                          <th rowSpan={2}>capsule_serial </th>
-                          <th rowSpan={2}>status</th>
-                          <th rowSpan={2}>mission</th>
-                         
-                      </tr>
-                      <tr>
-                          <th>landings</th>
-                          <th>type</th>
-                          <th>reuse_count</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      {
-                          this.state.capsules.map(capsule =>
-                              <tr key={capsule.capsule_id}> 
-                                  <th>{capsule.capsule_serial}</th>
-                                  <td>{capsule.status}</td>
-                                  <td>{capsule.mission}</td>
-                                  <td>{capsule.landings}</td>
-                                  <td>{capsule.type}</td>
-                                  <td>{capsule.reuse_count}</td>
-                              </tr>
-                          )
-                      }
-                  </tbody>
-              </table>
-      )
-    }
+
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th colSpan={7}>CAPSULES</th>
+        </tr>
+        <tr>
+          <th rowSpan={2}>capsule_id</th>
+          <th rowSpan={2}>capsule_serial </th>
+          <th rowSpan={2}>status</th>
+          <th rowSpan={2}>mission</th>
+        </tr>
+        <tr>
+          <th>landings</th>
+          <th>type</th>
+          <th>reuse_count</th>
+        </tr>
+      </thead>
+      <tbody>
+        {capsules.map((capsule) => (
+          <tr key={capsule.capsule_id}>
+            <td>{capsule.capsule_id}</td>
+            <td>{capsule.capsule_serial}</td>
+            <td>{capsule.status}</td>
+            <td>{capsule.mission}</td>
+            <td>{capsule.landings}</td>
+            <td>{capsule.type}</td>
+            <td>{capsule.reuse_count}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
