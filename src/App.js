b3e777ef77cac1a53f717eb6d5f26085fe45e202
Kilian Pichard <kil.pichard@icloud.com>
2022-10-05 10:26:10 +0200

    add switch with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import './App.css';
-import Launches from './components/Launches';
-import Capsules from './components/Capsules';
+import "./App.css";
+import Launches from "./components/Launches";
+import Capsules from "./components/Capsules";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 
-import React from 'react';
+import React from "react";
 
 function App() {
+  const [page, setPage] = React.useState("launches");
+
   return (
     <div className="App">
-      <Launches/>
-      <Capsules/>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>SpaceX</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link onClick={() => setPage("launches")}>Launches</Nav.Link>
+              <Nav.Link onClick={() => setPage("capsules")}>Capsules</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {page === "launches" && <Launches />}
+      {page === "capsules" && <Capsules />}
     </div>
   );
 }
